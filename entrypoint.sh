28beb54a20ae4fa79d285d2742734c2b54243023
Solomon Hykes <sh.github.6811@hykes.org>
2020-03-26 17:34:25 -0700

    push status + checkout
    Signed-off-by: Solomon Hykes <sh.github.6811@hykes.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -eu -o pipefail
 
 env
 
-[ -z "${INPUT_REPO_TOKEN}" ] && (echo "repo_token input missing"; exit 1)
-[ -z "${INPUT_BL_API_KEY}" ] && (echo "bl_api_key input missing"; exit 1)
-[ -z "${INPUT_BL_DOMAIN}" ] && (echo "bl_domain input missing"; exit 1)
-[ -z "${INPUT_BL_TARGET}" ] && (echo "bl_target input missing"; exit 1)
+[ -z "${INPUT_REPO_TOKEN:-}" ] && (echo "repo_token input missing"; exit 1)
+[ -z "${INPUT_BL_API_KEY:-}" ] && (echo "bl_api_key input missing"; exit 1)
+[ -z "${INPUT_BL_DOMAIN:-}" ] && (echo "bl_domain input missing"; exit 1)
+[ -z "${INPUT_BL_TARGET:-}" ] && (echo "bl_target input missing"; exit 1)
 
 # Grab PR info
 PR_DATA=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request)
@@ -24,11 +24,18 @@ PAYLOAD=$(
 		'setpath(["pr", $pr];{"status": "open"})'
 )
 
-bl push \
+bl draft init pr-push
+bl push --draft pr-push \
 	"${INPUT_BL_DOMAIN}" \
-	--kind json \
-	--target "${INPUT_BL_TARGET}" \
-	"$PAYLOAD"
+	--type text \
+	--target "${INPUT_BL_TARGET}.pr.status" \
+	"open"
+bl push --draft pr-push \
+	"${INPUT_BL_DOMAIN}" \
+	--type directory \
+	--target "${INPUT_BL_TARGET}.pr.branch.tip.checkout" \
+	.
+bl draft apply pr-push
 
 # If we're not in a pull request, stop here
 if [ "$PR_DATA" = "null" ]; then
